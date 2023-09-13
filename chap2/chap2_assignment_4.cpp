56858e3f67a292440dce6c6afacb14162342dbf4
임규연 <lky473736@icloud.com>
2023-09-13 12:02:21 +0900

    Create chap2_assignment_4.cpp

---
+++ chap2/chap2_assignment_4.cpp
@@ -0,0 +1,14 @@
+// 0913 과제 
+
+#include <stdio.h>
+
+int main()
+{
+    char ch = 300;
+    printf ("ch = %c, %d\n", ch, ch);
+}
+
+// ch = , , 300
+
+// error 1) ch는 한 문자가 아니라 %c로 받을 수 없다.
+// error 2) %d는 왜 안될까?
