66de3f883dec9a22811bb4b296aa4b0f7b51855b
임규연 <lky473736@icloud.com>
2023-09-13 12:04:05 +0900

    Create chap2_assignment_5.cpp

---
+++ chap2/chap2_assignment_5.cpp
@@ -0,0 +1,31 @@
+// 0913 과제 2
+
+#include <stdio.h>
+
+int main()
+{
+    char ch1 = ',';
+    printf ("ch = %d\n', ch1);
+}
+
+/*
+  
+input
+stderr
+Compilation failed due to following error(s).main.c: In function ‘main’:
+main.c:8:13: warning: missing terminating " character
+    8 |     printf ("ch = %d\n', ch1);
+      |             ^
+main.c:8:13: error: missing terminating " character
+    8 |     printf ("ch = %d\n', ch1);
+      |             ^~~~~~~~~~~~~~~~~~
+main.c:9:1: error: expected expression before ‘}’ token
+    9 | }
+      | ^
+main.c:8:13: error: expected ‘;’ before ‘}’ token
+    8 |     printf ("ch = %d\n', ch1);
+      |             ^
+      |             ;
+    9 | }
+      | ~            
+      */
