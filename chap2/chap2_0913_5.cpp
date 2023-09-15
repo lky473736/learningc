077ec01ab29540ac144a80167142dc95cb884062
임규연 <lky473736@icloud.com>
2023-09-15 22:36:40 +0900

    Create chap2_0913_5.cpp

---
+++ chap2/chap2_0913_5.cpp
@@ -0,0 +1,16 @@
+# 0913 실습 5
+
+  #include <stdio.h>
+
+  int main()
+{
+  int i;
+  char x;
+
+  scanf_s ("%3d%*5d%c", &i, &x, 1); 
+
+  printf ("i = %d\n", i);
+  printf ("x = %d\n", x);
+
+  return 0;
+}
