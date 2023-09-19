3ebbc27429485d04d7b2fd9271caa530ed2d18d3
임규연 <lky473736@icloud.com>
2023-09-19 09:26:36 +0900

    Update chap2_assignment_5.cpp

--- chap2/assignment/chap2_assignment_5.cpp
+++ chap2/assignment/chap2_assignment_5.cpp
@@ -5,29 +5,7 @@
 int main()
 {
     char ch1 = ',';
-    printf ("ch = %d\n', ch1);
+    printf ("ch = %d\n", ch1);
 }
 
-// 애초에 %d는 정수형을 출력하는 것인데 어떻게 출력할 수 있는가?
-
-/*
-  
-input
-stderr
-Compilation failed due to following error(s).main.c: In function ‘main’:
-main.c:8:13: warning: missing terminating " character
-    8 |     printf ("ch = %d\n', ch1);
-      |             ^
-main.c:8:13: error: missing terminating " character
-    8 |     printf ("ch = %d\n', ch1);
-      |             ^~~~~~~~~~~~~~~~~~
-main.c:9:1: error: expected expression before ‘}’ token
-    9 | }
-      | ^
-main.c:8:13: error: expected ‘;’ before ‘}’ token
-    8 |     printf ("ch = %d\n', ch1);
-      |             ^
-      |             ;
-    9 | }
-      | ~            
-      */
+// comma의 아스키 코드는 44
