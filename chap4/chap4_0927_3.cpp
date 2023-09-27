5f9a25c7aebe764bdde4acee6a039113faea03df
임규연 <lky473736@icloud.com>
2023-09-27 11:58:42 +0900

    Create chap4_0927_3.cpp

---
+++ chap4/chap4_0927_3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    
+    for (i = 1; i < 6; i = i + 1)
+    {
+        switch (i) 
+        {
+            case 1 :
+                printf ("odd\n");
+                break;
+            case 3 :
+                printf ("odd\n");
+                break;
+            case 5 :
+                printf ("odd\n");
+                break;
+            default :
+                printf ("even\n");
+        }
+    }
+    return 0;
+}
