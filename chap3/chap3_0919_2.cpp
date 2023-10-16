be9fca8c00276d28771463d96186641191956774
임규연 <lky473736@icloud.com>
2023-10-16 14:07:04 +0000

    Update chap3_0919_2.cpp

--- chap3/chap3_0919_2.cpp
+++ chap3/chap3_0919_2.cpp
@@ -6,11 +6,11 @@ int main()
 {
     int i, j = 6;
     
-    i = j++ + 3;
-    PRN (i, j);
+    i = j++ + 3; 
+    PRN (i, j); // i = 9, j = 7
     
-    i = ++j;
-    PRN (i, j);
+    i = ++j; 
+    PRN (i, j); // i = 8, j = 8
     
     return 0;
 }
