d3135d0177a912e2439fff8ea638dd48bc4f5f34
임규연 <lky473736@icloud.com>
2023-10-16 13:46:53 +0000

    Update chap2_0913_2.cpp

--- chap2/chap2_0913_2.cpp
+++ chap2/chap2_0913_2.cpp
@@ -11,5 +11,7 @@ int main()
     printf ("%d\n", val);
     
     val = n;
-    printf ("%d\n", val);
+    printf ("%d\n", val); // 열거형 enum은 수에 대한 indexing이 전의 indexing보다 하나씩 늘어나는 특징 지님.
+    // 예를 들어 enum example {k = 1, b, s = 4, p}
+    // 여기서 b = 2, p = 5
 }
