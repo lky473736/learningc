ad2cd578474cc84d3d71db3ed4e0b80dea1d62f6
임규연 <lky473736@icloud.com>
2023-10-16 13:54:47 +0000

    Update chap2_0913_5.cpp

--- chap2/chap2_0913_5.cpp
+++ chap2/chap2_0913_5.cpp
@@ -1,16 +1,16 @@
 // 0913 실습 5
 
-  #include <stdio.h>
+#include <stdio.h>
 
-  int main()
+int main()
 {
   int i;
   char x;
 
-  scanf_s ("%3d%*5d%c", &i, &x, 1); 
+  scanf ("%3d%*5d%c", &i, &x); // 3자리를 i로 받고, 5자리 건너뒤고, 문자 하나 받기
 
   printf ("i = %d\n", i);
-  printf ("x = %d\n", x);
+  printf ("x = %c\n", x);
 
   return 0;
 }
