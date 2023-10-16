de9a69c414725b7f9e5d51fc4879b0c87dbf2bfc
임규연 <lky473736@icloud.com>
2023-10-16 14:08:31 +0000

    Update chap3_0919_4.cpp

--- chap3/chap3_0919_4.cpp
+++ chap3/chap3_0919_4.cpp
@@ -4,12 +4,12 @@ int main()
 {
     int i = 6 + 31;
     double j = 3.1 * 2.0;
-    char k = 'k' + 1;
+    char k = 'k' + 1; // k = l
     
     i %= 6; // i = i % 6
     
     printf ("%d\n", i);
-    printf ("%f\n", j);
+    printf ("%f\n", j); // 6.200000
     printf ("%c", k);
     
     return 0;
