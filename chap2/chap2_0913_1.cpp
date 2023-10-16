8029e6f57fc3b1e415f2ccd0a27a31e5b1a42433
임규연 <lky473736@icloud.com>
2023-10-16 13:45:38 +0000

    Update chap2_0913_1.cpp

--- chap2/chap2_0913_1.cpp
+++ chap2/chap2_0913_1.cpp
@@ -5,12 +5,12 @@
 int main()
 {
     int i, today;
-    enum today {SUN, MON, TUE, WEN, THU, FRI, SAT};
+    enum today {SUN, MON, TUE, WEN, THU, FRI, SAT}; // enum 이름 {변수들};
     
     today = SUN;
     printf ("%d\n", today);
     
-    for (i = 0; i <= SAT; i++)
+    for (i = 0; i <= SAT; i++) // SAT까지니깐 6까지 출력한다는 것
     {
         printf ("%d\n", i);
     }
