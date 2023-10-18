6ef2d32ec18aed2ce2e0495c745309af69c17b06
임규연 <lky473736@icloud.com>
2023-10-18 15:24:20 +0900

    Create chap7_1018_11.cpp

---
+++ chap7/chap7_1018_11.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char k[101010]; // 기억장소 6칸 + 1칸(널문자) 가짐 
+    // G a c h o n \0 
+    
+    int i = 0, *p;
+    
+    while ((k[i++] = getchar()) != '\n')
+    {
+        for (int j = 0; j < i; j++)
+        {
+            putchar (k[j]);
+        }
+    }
+}
