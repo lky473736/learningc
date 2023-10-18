06c20b33dbe77be7b79b03e76d51e435122aa6c1
임규연 <lky473736@icloud.com>
2023-10-18 15:23:25 +0900

    Create chap7_1018_9.cpp

---
+++ chap7/chap7_1018_9.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char k[] = "Gachon"; // 기억장소 6칸 + 1칸(널문자) 가짐 
+    // G a c h o n \0 
+    
+    int i, *p;
+    
+    p = k;
+    
+    for (i = 0; i < 7; i++)
+    {
+        printf ("k[%d] = %c, addr = %p\n", i, k[i], (k + i)); // char의 byte가 1이니깐 하나씩 주소 차이
+    }
+    
+    for (i = 0; i < 7; i++)
+    {
+        putchar (k[i]); // 개행 없음
+    }
+    
+    printf ("\n");
+    puts (k); // 문자열 출력
+}
