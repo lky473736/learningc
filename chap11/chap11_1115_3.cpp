f6c54a405db4f16f51e5d2fb91d0154fd486c335
임규연 <lky473736@icloud.com>
2023-11-15 22:17:55 +0900

    Create chap11_1115_3.cpp

---
+++ chap11/chap11_1115_3.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+union list {
+    int a;
+    char b; // 기억장소를 공유함
+};
+
+int main()
+{
+    typedef int jungsu; // 새로운 자료형 타입 만듬 (jungsu라는 자료형)
+    
+    union list k;
+    k.a = 97;
+    printf ("%d %c\n", k.a, k.b); // k.a랑 k.b가 동일한 값을 지니면서 97 a 출력 
+    k.a = 100;
+    printf ("%d %c\n", k.a, k.b);
+    
+    jungsu m = 37; // jungsu == int으로 형선언
+    printf ("%d", m);
+}
