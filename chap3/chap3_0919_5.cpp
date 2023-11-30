d65ea57685c2bc0da5c4f08a947bc05fecf899de
임규연 <lky473736@icloud.com>
2023-12-01 00:34:45 +0900

    review

--- chap3/chap3_0919_5.cpp
+++ chap3/chap3_0919_5.cpp
@@ -10,3 +10,9 @@ int main()
     int k = 'a'; // 'a'에 대한 아스키 코드를 k에 저장
     printf ("%d", k);
 }
+
+/* 복습
+부정 연산자는 boolean expression의 진리값을 부정시켜준다.
+0은 False, 1은 True를 의미하며
+C언어 계열에서는 보통은 0이 아니면 다 참으로 인식한다.
+*/
