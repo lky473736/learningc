2328814dac386a14645988949e011bdae4bf8c71
임규연 <lky473736@icloud.com>
2023-11-30 23:59:14 +0900

    review

--- chap2/chap2_0913_1.cpp
+++ chap2/chap2_0913_1.cpp
@@ -17,3 +17,19 @@ int main()
     
     return 0;
 }
+
+/* 복습
+열거형은 아래와 같이 사용한다.
+
+열거형 변수를 지정할 변수 선언;
+enum 열거형명 {열거형 변수};
+열거형 변수를 지정할 변수 = 특정 열거형 변수;
+
+예를 들어,
+
+int daramji;
+enum food {milk, dotori, gamja};
+daramji = milk;
+
+그러면 daramji에는 milk에 대응되는 숫자 0이 저장된다. 
+*/
