6d89e22ec3595226adc68996e1593b84ab27e24a
임규연 <lky473736@icloud.com>
2023-12-01 03:00:20 +0900

    review

--- chap6/chap6_1011_3.cpp
+++ chap6/chap6_1011_3.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define MUL1(i) i * i 
-#define PRN(i) printf ("%d\n", i);
+#define PRN(i) printf ("%d\n", i)//;
 
 int main()
 {
@@ -10,3 +10,7 @@ int main()
     PRN(MUL1(3));
     PRN(3 / MUL1(3));
 }
+
+/* 복습
+#define에는 세미콜론 안붙이니깐 조심하자. 
+*/
