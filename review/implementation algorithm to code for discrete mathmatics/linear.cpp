343d13ca789dcfb20967d405aceda96ecf6b1369
임규연 <lky473736@icloud.com>
2023-11-30 13:25:11 +0900

    Create linear.cpp

---
+++ review/implementation algorithm to code for discrete mathmatics/linear.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int linear(int x, int arr[10]);
+
+int main()
+{
+    printf ("linear search algorithm\n");
+    
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int location = linear(9, arr);
+    
+    printf ("location : %d", location);
+}
+
+int linear (int x, int arr[10])
+{
+    int k = 0;
+    int location = -1;
+    
+    while (k <= 10) 
+    {
+        if (arr[k] == x)
+        {
+            location = k;
+        }
+        
+        k = k + 1;
+    }
+    
+    return location;
+}
