dce52d269f65fa94cd951c2b98ed41821d9ee19f
임규연 <lky473736@icloud.com>
2023-12-01 02:42:30 +0900

    review

--- chap5/chap5_1010_6.cpp
+++ chap5/chap5_1010_6.cpp
@@ -11,3 +11,22 @@ int main()
     
     return 0;
 }
+
+/* 복습
+이 코드도 전의 코드랑 마찬가지. 개행 문자를 처리해주기 위해선 아래와 같은 처리를 해주어야 한다.
+
+    #include <stdio.h>
+
+int main()
+{
+    char c;
+    
+    while ((c = getchar()) != '\n') // EOF
+    {
+        int _ = getchar();
+        putchar(c);
+    }
+    
+    return 0;
+}
+*/
