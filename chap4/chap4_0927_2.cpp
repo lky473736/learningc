e7abdb92e66958f63ea66fc202a71bc7fefa7b98
임규연 <lky473736@icloud.com>
2023-12-01 00:51:34 +0900

    review

--- chap4/chap4_0927_2.cpp
+++ chap4/chap4_0927_2.cpp
@@ -20,3 +20,13 @@ int main()
     }
     return 0;
 }
+
+/* 복습
+switch문에서 break를 사용하지 않으면, 해당 케이스에 변수 대응이 성공할 시에 그 이하의 영역까지 작동한다. 
+위 코드로 예를 들었을 때, 
+
+i = 1이면 case 1에 대응 성공 -> odd, odd, odd, even 출력
+i = 2이면 아무 case에도 대응 안됨 -> even 출력
+i = 3이면 case 3에 대응 성공 -> odd, odd, even 출력 
+...
+*/
