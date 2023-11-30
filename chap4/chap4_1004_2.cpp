7fe6d65e54ea6359107cc8412d96b45a03fc357a
임규연 <lky473736@icloud.com>
2023-12-01 01:06:25 +0900

    review

--- chap4/chap4_1004_2.cpp
+++ chap4/chap4_1004_2.cpp
@@ -18,3 +18,50 @@ k:  i += 1;
 }
 
 // 이걸 for문, while문, do-while문으로 바꿀 수 있어야 함.
+
+/* 복습
+(for문)
+#include <stdio.h>
+
+int main()
+{
+    int suma = 0;
+    
+    for (int i = 1; i <= 10; i++)
+    {
+        suma += i;
+    }
+    
+    printf ("%d", suma);
+}
+
+(while문)
+#include <stdio.h>
+
+int main()
+{
+    int suma = 0, i = 1;
+    
+    while (i <= 10)
+    {
+        suma += i;
+        i++;
+    }
+    
+    printf ("%d", suma);
+}
+
+(do-while문)
+#include <stdio.h>
+
+int main()
+{
+    int suma = 0, i = 1;
+    
+    do
+    {
+        suma += i++;
+    } while (i <= 10);
+    
+    printf ("%d", suma);
+}
