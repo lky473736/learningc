99b7c0c6eb108ee41f5ad22aab15b8fd4ad56f33
임규연 <lky473736@icloud.com>
2023-12-02 19:08:31 +0900

    review

--- chap8/chap8_1031_3.cpp
+++ chap8/chap8_1031_3.cpp
@@ -15,3 +15,78 @@ int main()
 }
 
 // do-while문, while문, goto문으로도 표현해보기 
+
+/* 복습
+    
+// for문
+#include <stdio.h>
+
+int main()
+{
+    int k[3] = {10, 20, 30}, *p;
+    p = k;
+    
+    int suma = 0;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        suma += *(p + i);
+    }
+    printf ("%d", suma);
+}
+
+// while문
+#include <stdio.h>
+
+int main()
+{
+    int k[3] = {10, 20, 30}, *p;
+    p = k;
+    int i = 0, suma = 0;
+    
+    while (i < 3)
+    {
+        suma += *(p + i);
+        i += 1;
+    }
+    
+    printf ("%d", suma);
+}
+
+// do-while문
+#include <stdio.h>
+
+int main()
+{
+    int k[3] = {10, 20, 30}, *p;
+    p = k;
+    int i = 0, suma = 0;
+    
+    do
+    {
+        suma += *(p + i++);
+    } while (i != 3);
+    
+    printf ("%d", suma);
+}
+
+// goto문
+#include <stdio.h>
+
+int main()
+{
+    int k[3] = {10, 20, 30}, *p;
+    p = k;
+    int i = 0, suma = 0;
+    
+K:  suma += *(p + i);
+    i++;
+    
+    if (i != 3)
+    {
+        goto K;
+    }
+    
+    printf ("%d", suma);
+}
+*/
