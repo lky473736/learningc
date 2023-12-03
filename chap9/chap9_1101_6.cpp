f5d9f0fff41547e99c8121142900d339958d2687
임규연 <lky473736@icloud.com>
2023-12-04 01:14:01 +0900

    review

--- chap9/chap9_1101_6.cpp
+++ chap9/chap9_1101_6.cpp
@@ -16,3 +16,32 @@ void ex (int *v1, int *v2)
     *v1 = *v2; // v1 = 6
     *v2 = z; // v2 = 5
 }
+
+/* 복습
+위 코드는 두 변수의 값을 서로 교환하는 exchange의 과정을 하고 있다. 15번째 줄에 z는 원래 v1의 값을 저장하는 과정을 하고 있다.
+아래 코드는 연습삼아 세 변수의 값을 교환하는 함수를 작성한 것이다. 변수 1은 변수 2와, 변수 2는 변수 3과, 변수 3은 변수 1과 교환된다.
+
+#include <stdio.h>
+
+void exchange_3 (int *v1, int *v2, int *v3);
+
+int main()
+{
+    int a = 0, b = 1, c = 2;
+    
+    exchange_3 (&a, &b, &c);
+    
+    printf ("%d %d %d", a, b, c);
+}
+
+void exchange_3 (int *v1, int *v2, int *v3)
+{
+    int r;
+    r = *v3;
+
+    *v3 = *v2;
+    *v2 = *v1;
+    *v1 = r;
+}
+*/
+
