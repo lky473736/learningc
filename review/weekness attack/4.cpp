22dabe8270e1a82a0862786907c1224682fdd05e
임규연 <lky473736@icloud.com>
2023-12-04 19:02:25 +0900

    Create 4.cpp

---
+++ review/weekness attack/4.cpp
@@ -0,0 +1,14 @@
+// exercise 4. 문자열 표현
+
+#include <stdio.h>
+#define A "daramji"
+
+int main()
+{
+    char B[] = "daramji";
+    char *C = "daramji";
+    
+    printf ("%s\n", A);
+    printf ("%s\n", B);
+    printf ("%s", C);
+}
